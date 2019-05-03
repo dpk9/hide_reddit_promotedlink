65b72e218658f44b2e35dbf6bacbf4f6a9403d39
David Kalish <davidpkalish@gmail.com>
2019-05-03 14:01:28 -0400

    Create Hide_Reddit_promotedlink.user.js

---
+++ Hide_Reddit_promotedlink.user.js
@@ -0,0 +1,25 @@
+// ==UserScript==
+// @name     Hide Reddit's promoted posts
+// @namespace hide_Reddits_promoted_posts
+// @description Hide Reddit's promoted posts
+// @include  https://www.reddit.com/
+// @include  https://www.reddit.com/r/*
+// @version  1
+// @grant    none
+// ==/UserScript==
+
+// function mark_ads() {
+//     let spans = Array.filter(document.getElementsByTagName("span"), (span) => span.textContent == "promoted")
+//     spans.forEach((span) => span.parentElement.parentElement.parentElement.parentElement.style = "background-color: yellow")
+// }
+
+function hide_ads() {
+    let promoted_links = document.getElementsByClassName("promotedlink")
+    for (let promo of promoted_links) {
+        promo.style.display = "none"
+    }
+}
+
+hide_ads()
+//Also rerun the code each time document change (i.e new posts are added when user scroll down)
+document.addEventListener("DOMNodeInserted", hide_ads)
